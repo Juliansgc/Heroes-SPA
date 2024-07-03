1d8de040b74fe220eed9a3c1aec10c0993577314
Julian Smith Galindo Cortes <julians.galindoc@gmail.com>
2024-07-03 11:39:03 -0500

    Iniciando el proyecto

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { HeroresRoutes } from '../heroes'
+import { LoginPage } from '../auth'
+export const AppRouter = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="login" element={ <LoginPage />} />
+                <Route path="/*" element={ <HeroresRoutes />} />
+            </Routes>
+        </>
+    )
+}
